3ecd28b6c25f4b1bcf2e546e045b93dc2b1f199a
GioAsanidze <gioasanidze77@gmail.com>
2025-05-09 21:15:09 +0400

    commit

---
+++ Day 4/Homework/lesson4.py
@@ -0,0 +1,24 @@
+#დავალება N1  
+Name = input()
+age = input()
+print(f"Name: {Name}, Age: {age}")
+
+#დავალება N2
+height = int(input())
+width = int(input())
+S = height * width 
+P = height * 2 + width * 2
+print(S)
+print(P)
+
+#დავალება N3
+#input არის როცა კომპიუტერი მიიღებს რაიმეს ადამიანებისგან.
+#output არის როცა კომპიუტერიუშვებს რაიმეს ადამიანებისთვის.
+
+#დავალება N4
+First_number = int(input())
+Second_number = int(input())
+print( First_number + Second_number )
+print( First_number - Second_number ) 
+print( First_number * Second_number ) 
+print( First_number / Second_number ) 
