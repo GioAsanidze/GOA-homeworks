c06c3d9ea9ab4ceba951bc2012aeeab395cc5684
GioAsanidze <gioasanidze77@gmail.com>
2025-05-15 19:20:36 +0400

    commit

---
+++ Day 6/Homework/lesson6.py
@@ -0,0 +1,36 @@
+#დავალება N1
+#sequence არის ის როცა კომპიუტერი მიყვება ზემოდან ქვემოთ.
+#iteration არის იგივე ლუპი, ანუ როცა რაღაც მეორდება. 
+#selection არის როცა ვირჩევთ, თუ რომელი კოდი შევასრულოთ.
+
+#დავალება N2
+#ალგორითმი არის იგივე ინსტრუქცია რომელსაც უნდა მივყვეთ, მაგალითად ყიყლიყოს რეცეპტი ან სხვა.
+
+#დავალება N3 
+#print(True or False and False or True and False or False and False or False and True and False or True)
+#print(5 > 10 or 7 * 7 / 7 == 7 and False or True and "1234" != "1234" and 7 + 3 * 3 + 1 == 15 and True and True or 100 > 100 or True)
+# პირველი იზამს True, ხოლო მეორე იზამს True.
+
+#დავალება N4
+number = int(input()) 
+if number % 2 == 0 and number > 10 or number == 7:
+    print( "True")
+
+#დავალება N5 
+num_1 = int(input())
+num_2 = int(input())
+num_3 = int(input())
+string_1 = str(10)
+string_2 = str(100)
+string_3 = str(1000)
+float_1 = float(10)
+float_2 = float("50")
+float_3 = float(500)
+boolean_1 = True
+boolean_2 = False 
+boolean_3 = 1743 > 312
+
+#დავალება N6
+year = int(input())
+if year % 4 == 0:
+    print("This is leap year")
